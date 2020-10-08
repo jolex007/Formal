3ab53d3d37a8b5a6e3b05936764b24972eae5b67
jolex007 <jokers3000@gmail.com>
2020-10-08 04:29:21 +0300

    Added tests

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+import pytest
+from StateMachine import StateMachine, readStateMichineFromFile, saveStateMachineToText, Alphabet
+from MinimizeStateMachine import minimizeStateMachine
+
+class TestStateMachine:
+    @pytest.mark.parametrize('machine_filename, answer_filename', [
+        ('tests/machines/machine_1.txt', 'tests/machines/machine_2.txt'),
+        ('tests/machines/machine_3.txt', 'tests/machines/machine_4.txt')
+    ])
+    def test_minimizer(self, machine_filename: str, answer_filename: str):
+        machine = readStateMichineFromFile(machine_filename)
+        
+        minMachine = minimizeStateMachine(machine)
+
+        minMachineText = saveStateMachineToText(minMachine)
+
+        f = open(answer_filename)
+        assert minMachineText == f.read()
+        pass
+
+    pass
