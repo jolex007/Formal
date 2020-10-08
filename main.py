dd9bf52dbd72dd4b16a3b8dade614a794daeb329
jolex007 <jokers3000@gmail.com>
2020-10-08 04:29:21 +0300

    Added tests

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from StateMachine import StateMachine, Alphabet
+from StateMachine import readStateMichineFromFile, saveStateMachineToText
 from StateMachineViz import vizualizeStateMachine
 from MinimizeStateMachine import minimizeStateMachine
 
 def main():
-    machine = StateMachine(alphabet=Alphabet(['0', '1']),
-                           numberOfStates=8,
-                           begState=0,
-                           endStates=[5, 6],
-                           switches=[[(7, '0'), (1, '1')],
-                                     [(7, '0'), (0, '1')],
-                                     [(4, '0'), (5, '1')],
-                                     [(4, '0'), (5, '1')],
-                                     [(5, '0'), (6, '1')],
-                                     [(5, '0'), (5, '1')],
-                                     [(6, '0'), (5, '1')],
-                                     [(2, '0'), (1, '1')]]
-                                     )
+    machine = readStateMichineFromFile('tests/machines/machine_3.txt')
 
     minMachine = minimizeStateMachine(machine)
+
+    text = saveStateMachineToText(minMachine)
+    print(text)
 
     vizualizeStateMachine(machine, 'before')
     vizualizeStateMachine(minMachine, 'after')
